939ca586198469c668824cc5d4b9c0cf74a8edf1
eFacorro <facorroesteiro@gmail.com>
2024-01-17 00:35:46 +0100

    creado carga y visualizacion de usuarios

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -13,16 +13,86 @@ const comunicandoServer = async (datos)=>{
 function creoTarjeta(usuario){
   let ref = document.querySelector(".lista-usuarios");
   let form = document.createElement("form");
+  let img = document.createElement("img");
+  img.setAttribute("src", "./imgs/default.png")
   
   let user = document.createElement("input");
   user.setAttribute("name", "user");
   user.setAttribute("type", "text");
   user.setAttribute("placeholder", "Usuario");
+  user.disabled = "true";
   user.value = usuario.user;
 
+  let pwd = document.createElement("input");
+  pwd.setAttribute("name", "pwd");
+  pwd.setAttribute("type", "password");
+  pwd.setAttribute("placeholder", "Contraseña");
+  pwd.disabled = "true";
+  pwd.value = usuario.pwd;
+
+  let nombre = document.createElement("input");
+  nombre.setAttribute("name", "nombre");
+  nombre.setAttribute("type", "text");
+  nombre.setAttribute("placeholder", "Nombre");
+  nombre.disabled = "true";
+  if(usuario.nombre != undefined){
+    nombre.value = usuario.nombre;
+  }
+  
+  let primerApellido = document.createElement("input");
+  primerApellido.setAttribute("name", "primerApellido");
+  primerApellido.setAttribute("type", "text");
+  primerApellido.setAttribute("placeholder", "Primer Apellido");
+  primerApellido.disabled = "true";
+  if(usuario.primerApellido != undefined){
+    primerApellido.value = usuario.primerApellido;
+  }
+
+  let segundoApellido = document.createElement("input");
+  segundoApellido.setAttribute("name", "segundoApellido");
+  segundoApellido.setAttribute("type", "text");
+  segundoApellido.setAttribute("placeholder", "Segundo Apellido");
+  segundoApellido.disabled = "true";
+  if(usuario.segundoApellido != undefined){
+    segundoApellido.value = usuario.segundoApellido;
+  }
+
+  let fechaNacimiento = document.createElement("input");
+  fechaNacimiento.setAttribute("name", "fechaNacimiento");
+  fechaNacimiento.setAttribute("type", "date");
+  fechaNacimiento.disabled = "true";
+  if(usuario.fechaNacimiento != undefined){
+    fechaNacimiento.value = usuario.fechaNacimiento;
+  }
+
   ref.appendChild(form)
+  form.appendChild(img);
   form.appendChild(user);
-  console.log(usuario.user)
+  form.appendChild(pwd);
+  form.appendChild(nombre);
+  form.appendChild(primerApellido);
+  form.appendChild(segundoApellido);
+  form.appendChild(fechaNacimiento);
+  
+  let div = document.createElement("div");
+
+  let edit = document.createElement("input");
+  edit.setAttribute("type", "button");
+  edit.value = "Editar";
+
+  let save = document.createElement("input");
+  save.setAttribute("type", "button");
+  save.value = "Guardar";
+
+  let del = document.createElement("input");
+  del.setAttribute("type", "button");
+  del.value = "Borrar";
+  
+  form.appendChild(div);
+  div.appendChild(edit);
+  div.appendChild(save);
+  div.appendChild(del);
+
 }
 
 export {
