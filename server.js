e9c746f3e57b8bfdee48b1436dd87102b8c4bad5
eFacorro <facorroesteiro@gmail.com>
2024-01-26 21:18:30 +0100

    cambios menores

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const {
   enviarToken,
   borrarImg,
   checkToken } = require("./funcions.js");
-  
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
