af87c505fe069585faccd8f14342c81de5df476f
eFacorro <151563340+eFacorro@users.noreply.github.com>
2025-09-29 16:28:00 +0200

    Update funcNodemailer.js

--- funciones/funcNodemailer.js
+++ funciones/funcNodemailer.js
@@ -16,7 +16,7 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({    // https://youtu.be/TvOfrZnaunQ?t=127
 host: process.env.MAIL_HOST, // "stmp.google.com",
 port: process.env.MAIL_PORT,
-secure: true,
+// secure: true,
 auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.PASS,
